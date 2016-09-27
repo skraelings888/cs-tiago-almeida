8a2dbd247c28cffd3685186d9efd50b7249e2ecf
cs-tiago-almeida <tiago.almeida@concrete.com.br>
2016-09-27 16:25:33 -0300

    second commit

--- logs_clean.sh
+++ logs_clean.sh
@@ -13,5 +13,4 @@ find ${DIRNAME} -type f -mtime ${ZIPDAYS} -name "*.log*" -a ! -name "*.gz" -exec
 
 find ${DIRNAME} -type f -mtime ${ZIPDAYS} -name "*.out*" -a ! -name "*.gz" -exec gzip -f {} \;
 
-find ${DIRNAME} -type f -mtime ${PURGEDAYS} -name "*.gz" -exec rm -f {} \;
 
